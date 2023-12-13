f980cc3b44f9a537d76a8430bbc5e4c1881067c9
Eckson-Glitho <g.eckson03@gmail.com>
2023-12-13 18:17:40 +0100

    correction 3

--- explore.py
+++ explore.py
@@ -21,27 +21,27 @@ def import_data_Test() -> pd.DataFrame:
 
 data = import_data_Train()
 
-#Show the first rows to understand the data structure
-print(data.head())
+# #Show the first rows to understand the data structure
+# print(data.head())
 
-#information about data tyypes, missing values, etc
-print(data.info())
+# #information about data tyypes, missing values, etc
+# print(data.info())
 
-#
-print(data.describe())
-print(data['Survived'].value_counts())
+# #
+# print(data.describe())
+# print(data['Survived'].value_counts())
 
-sns.pairplot(data, hue='Survived')
-plt.show()
+# sns.pairplot(data, hue='Survived')
+# plt.show()
 
-sns.countplot(x='Survived', data=data)
-plt.title('Repartition des survivants')
-plt.show()
+# sns.countplot(x='Survived', data=data)
+# plt.title('Repartition des survivants')
+# plt.show()
 
-sns.countplot(x='Survived', hue='Sex', data=data)
-plt.title('Repartition des survivants par sexe')
-plt.show()
+# sns.countplot(x='Survived', hue='Sex', data=data)
+# plt.title('Repartition des survivants par sexe')
+# plt.show()
 
-sns.countplot(x='Survived', hue='Pclass', data=data)
-plt.title('Repartition des survivants par sexe')
-plt.show()
+# sns.countplot(x='Survived', hue='Pclass', data=data)
+# plt.title('Repartition des survivants par sexe')
+# plt.show()
--- test.py
+++ test.py
@@ -1,86 +1,34 @@
 import unittest
-import pandas as pd
 from process import transformation, import_data_Train
-import unittest
-import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, make_scorer
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.model_selection import cross_val_score
 
+def import_data():
+    # Implémentez la logique d'importation de données ici
+    test_data = import_data_Train()
+    return test_data
 
-class TestTransformations(unittest.TestCase):
-    def test_setUp(self):
-        # Setup code if needed
-        pass
+def test_transformation():
+    test_data = import_data()
+    transformed_data = transformation(test_data)
+    assert len(test_data) == len(transformed_data)
 
-    def test_test_transformation(self):
-        # Load test data
-        test_data = import_data_Train()
+def test_logistic_regression_accuracy():
+    test_data = import_data()
+    test_data_trans = transformation(test_data)
 
-        # Apply the transformation
-        transformed_data = transformation(test_data)
+    X_test = test_data_trans.drop("Survived", axis=1)
+    y_test = test_data_trans["Survived"]
+    X_train, X_test, y_train, y_test = train_test_split(X_test, y_test, test_size=0.3)
 
-        # Add your assertions here
-        self.assertEqual(len(test_data), len(transformed_data))
-        # Add more assertions based on your specific transformation logic
+    log_reg = LogisticRegression()
+    log_reg.fit(X_train, y_train)
+    y_pred = log_reg.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    assert 0.0 <= accuracy <= 1.0
 
-class TestLogisticRegression(unittest.TestCase):
-    def test_setUp(self):
-        # Load test data
-        test_data = import_data_Train()
 
-        # Apply the transformation
-        self.test_data_trans = transformation(test_data)
-
-        # Set up training and testing sets
-        X_test = self.test_data_trans.drop("Survived", axis=1)
-        y_test = self.test_data_trans["Survived"]
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X_test, y_test, test_size=0.3)
-
-        # Instantiate the logistic regression model
-        self.log_reg = LogisticRegression()
-
-    def test_test_logistic_regression_accuracy(self):
-        # Fit the model
-        self.log_reg.fit(self.X_train, self.y_train)
-
-        # Predict
-        y_pred = self.log_reg.predict(self.X_test)
-
-        # Check accuracy
-        accuracy = accuracy_score(self.y_test, y_pred)
-        self.assertTrue(accuracy >= 0.0 and accuracy <= 1.0)
-
-    def test_test_logistic_regression_confusion_matrix(self):
-        # Fit the model
-        self.log_reg.fit(self.X_train, self.y_train)
-
-        # Predict
-        y_pred = self.log_reg.predict(self.X_test)
-
-        # Check confusion matrix
-        confusion_mat = confusion_matrix(self.y_test, y_pred)
-        self.assertEqual(confusion_mat.shape, (2, 2))  # Assuming binary classification
-
-    def test_test_logistic_regression_classification_report(self):
-        # Fit the model
-        self.log_reg.fit(self.X_train, self.y_train)
-
-        # Predict
-        y_pred = self.log_reg.predict(self.X_test)
-
-        # Check classification report
-        classification_rep = classification_report(self.y_test, y_pred)
-        self.assertNotEqual(classification_rep, "")
-
-    def test_test_logistic_regression_cross_val_score(self):
-        # Cross-validate the model
-        scores = cross_val_score(self.log_reg, self.X_test, self.y_test, cv=5)
-
-        # Check if scores are within the expected range
-        for score in scores:
-            self.assertTrue(score >= 0.0 and score <= 1.0)
-
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
